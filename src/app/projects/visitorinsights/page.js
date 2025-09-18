9823300d33b6cbb60d47d718c5ecad107137e5e6
philgenb <philgenb@users.noreply.github.com>
2025-09-19 01:40:20 +0200

    Add visitor insights page

---
+++ src/app/projects/visitorinsights/page.js
@@ -0,0 +1,25 @@
+import ProjectDetailPage from "@/pages/ProjectDetailPage";
+import TechnologyCard from "@/components/TechnologyCard";
+import {ReactIcon} from "@/assets/imageComponents/technologies/ReactIcon";
+import {PythonIcon} from "@/assets/imageComponents/technologies/PythonIcon";
+import {FlaskIcon} from "@/assets/imageComponents/technologies/FlaskIcon";
+
+export default function VisitorInsights() {
+    return (
+        <ProjectDetailPage
+            title={`Visitor\nInsights`}
+            idea="VisitorInsights is a web application developed as part of my bachelor's thesis to support inclusive museum visits through predictive visitor information. It uses interpretable machine learning models to forecast daily crowd levels and presents results in an accessible, transparent way."
+            userexperience="The UI is designed for accessibility, featuring SHAP-based model explanations and visual/textual representations of uncertainty. It supports screen readers, keyboard navigation, and includes a high-contrast mode."
+            technology="Built with Flask and React in a modular MVCS architecture, VisitorInsights uses decision-based models like Random Forest and XGBoost, achieving 85% classification accuracy for daily predictions. The interface follows WCAG standards for a barrier-free web experience."
+            imageSubtitle="Predictive accessibility – powered by Flask, React & SHAP."
+            technologies={[
+                <TechnologyCard iconComponent={<ReactIcon />} bgColor="#FFE8EE" description={"React"} />,
+                <TechnologyCard iconComponent={<FlaskIcon />} bgColor="#DFE6FC" description={"Flask"} />,
+                <TechnologyCard iconComponent={<PythonIcon />} bgColor="#E8FFE9" description={"Accessible Web Design"} />
+            ]}
+            imgPath={"/assets/applications/VisitorInsights_Showcase.png"}
+        />
+
+
+    )
+}
