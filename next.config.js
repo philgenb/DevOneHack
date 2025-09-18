b8e73848e7ba990015637492af9a413a4ad17618
philgenb <philgenb@users.noreply.github.com>
2025-09-19 01:40:20 +0200

    update layout and update package.json build config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
 
-}
+  // Disable image optimization for static export
+  images: { unoptimized: true },
 
-module.exports = nextConfig
+  // Optional: trailingSlash: true,
+};
+
+module.exports = nextConfig;
